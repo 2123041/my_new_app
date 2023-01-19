e35e9ab37855b416ec76374e0f8f9fb0cd680094
2123041 <121269669+2123041@users.noreply.github.com>
2023-01-19 09:09:14 +0000

    1st

--- app.py
+++ app.py
@@ -1,32 +1,15 @@
 import streamlit as st
-import numpy as np
-from sklearn.linear_model import LinearRegression
 
-st.title("Simple Linear Regression App")
+st.title("ナップザック問題")
+st.text("商品の価値と重さを入力してください")
+st.text("重さ:")
+weights = st.text_input("")
+st.text("価値:")
+values = st.text_input("")
+st.text("ナップザックの最大重量を入力してください")
+max_weight = st.text_input("")
 
-st.write("This app finds the best line that fits a set of points.")
+if st.button("解く"):
+    result = knapsack(weights, values, max_weight)
+    st.success(f"最大価値は{result}です")
 
-# Creating the form to get inputs
-x = st.slider("x", 0, 10, 5)
-y = st.slider("y", 0, 10, 5)
-
-# Creating the linear regression model
-model = LinearRegression()
-
-# Fitting the model
-X = np.array(x).reshape(-1, 1)
-Y = np.array(y).reshape(-1, 1)
-model.fit(X, Y)
-
-# Predicting the y value
-y_pred = model.predict([[x]])[0][0]
-
-# Plotting the points
-st.write("Points")
-st.scatter_chart(pd.DataFrame({'x':x, 'y':y}))
-
-# Plotting the line
-st.write("Line")
-st.line_chart(pd.DataFrame({'x':x, 'y':y_pred}))
-
-st.write("y = {:.2f}x + {:.2f}".format(model.coef_[0][0], model.intercept_[0]))
